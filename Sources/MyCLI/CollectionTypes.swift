6023f7fe21f967d6d84bfcd06b9b3c188665769c
Hayato <7hyt0512@gmail.com>
2026-08-28 22:20:10 +0900

    Setを学習

--- Sources/MyCLI/CollectionTypes.swift
+++ Sources/MyCLI/CollectionTypes.swift
@@ -1,8 +1,8 @@
-enum CollectionTypes{
+enum CollectionTypes {
     static func learn() {
-       
         
-        var scores:[String: Int] = ["math": 80, "English" : 50]
+        
+        var scores: [String: Int] = ["math": 80, "English": 50]
         //var scores = ["math": 80, "English" : 50]
         
         scores["Japanese"] = 100
@@ -30,13 +30,32 @@ enum CollectionTypes{
         // 回してみた！
         for (key, value) in scores {
             print(key, value)
-            
-            /* 結果
-             (key: "English", value: 50)
-             (key: "math", value: 80)
-             (key: "Japanese", value: 100)
-             */
         }
+        
+        
+        //Set
+        var langs: Set = ["Swift", "Dart", "Swift"]
+        print(langs.count)
+        print(langs)
+        
+        let insertedLanguages1 = langs.insert("JavaScript")
+        print(insertedLanguages1)
+        
+        let insertedLanguages2 = langs.insert("Swift")
+        print(insertedLanguages2)
+        
+        let hasSwift = langs.contains("Swift")
+        print(hasSwift)
+        
+        
+        
+        let num1: Set = [1, 2, 3, 4, 5]
+        let num2: Set = [2, 4, 6, 8, 10]
+        
+        print(num1.union(num2).sorted())
+        print(num1.intersection(num2).sorted())
+        print(num1.subtracting(num2).sorted())
+        print(num1.symmetricDifference(num2).sorted())
         
     }
 }
