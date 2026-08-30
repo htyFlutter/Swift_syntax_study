00c6c94c7735f5e69ead6da5a8febac393e6b0fe
Hayato <7hyt0512@gmail.com>
2026-08-30 20:37:09 +0900

    Functions制覇ああああああ

--- Sources/MyCLI/Functions.swift
+++ Sources/MyCLI/Functions.swift
@@ -2,10 +2,17 @@ enum Functions {
     static func learn() {
         let result = add(a: 3, b: 1)
         print("答えは\(result)")
+        
         print(greet(to: "はやと"))
         print(greet(to: "はやと", greeting: "ういっす"))
+        
         print(calc(10))
         
+        let resultNum = minMax([1, 3, 4, 6, 7, 0])
+        print("最小: \(resultNum.min)、最大: \(resultNum.max)")
+        
+        let (minimumNum, maximumNum) = minMax([1, 4, 5, 19, 9, 0])
+        print(minimumNum, maximumNum)
     }
     
     static func add(a: Int, b: Int) -> Int {
@@ -18,6 +25,17 @@ enum Functions {
     
     static func calc(_ x: Int) -> Int {
         return x * 2
+    }
+    
+    
+    static func minMax(_ numbers:[Int]) -> (min: Int, max: Int) {
+        var currentMin = numbers[0]
+        var currentMax = numbers[0]
+        for num in numbers {
+            if num < currentMax { currentMin = num }
+            if num > currentMax { currentMax = num }
+        }
+        return (currentMin, currentMax)
     }
     
 }
