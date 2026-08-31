75c9b4647cf588a378fcfb6fa1327460d7b0e773
Hayato <7hyt0512@gmail.com>
2026-08-31 21:03:48 +0900

    minMaxの比較バグ修正とコメント追加

--- Sources/MyCLI/Functions.swift
+++ Sources/MyCLI/Functions.swift
@@ -27,14 +27,19 @@ enum Functions {
         return x * 2
     }
     
-    
+    //(int型リストを受け取る)
     static func minMax(_ numbers:[Int]) -> (min: Int, max: Int) {
+        //とりあえず1番最初の数字を最大最小の数(チャンピオン)に決めとく
         var currentMin = numbers[0]
         var currentMax = numbers[0]
+        
+        //リストで判断して...
         for num in numbers {
-            if num < currentMax { currentMin = num }
+            //1番最初の数よりリストの数より小さかったら(大きかったら)チャンピオン交代
+            if num < currentMin { currentMin = num }
             if num > currentMax { currentMax = num }
         }
+        //チャンピオンの数の結果を返す
         return (currentMin, currentMax)
     }
     
