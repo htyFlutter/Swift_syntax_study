1c3d8784bda74f20c316a9a4a94f9f3f2529ddb6
Hayato <7hyt0512@gmail.com>
2026-09-12 20:51:51 +0900

    MyCLI.swiftの中身スッキリさせた！

--- Sources/MyCLI/MyCLI.swift
+++ Sources/MyCLI/MyCLI.swift
@@ -43,78 +43,25 @@ struct Player {
 
 struct Weapon {
     var name: String
-    var maker: String?
+    var maker: Maker?
 }
 struct Hero {
     var name: String
     var weapon: Weapon?
 }
 
+struct Maker {
+    var company: String
+}
+
 @main
 struct MyCLI {
     static func main() {
         
-        let armed = Hero(name: "はやと", weapon: Weapon(name: "鉄砲", maker: nil))
-        let name1 = armed.weapon?.name ?? "素手"
-        let makerName = armed.weapon?.maker ?? "製造元不明"
-        print("\(armed.name)の武器: \(name1), 製造元: \(makerName)")
-        
-        let barehand = Hero(name: "ビギナー", weapon: nil)
-        let name2 = barehand.weapon?.name ?? "素手"
-        print("\(barehand.name)の武器: \(name2)")
-        
-        
-        //let a = Animal(name: "なにか")
-        //a.speak()
-        
-        //let d = Dog(name: "Pochi", breed: "Shiba")
-        //?d.speak()
-        
-        
-        /*
-        let p = Player(name: "test", hp: -1) ?? Player(name: "予備", hp: 50)
-        let status = p!.hp > 30 ? "元気" : "GAME OVER"
-        print("\(p!.name): 状態: \(status)")
-        
-        if let ng = Player(name: "バグ", hp: -100) {
-            print("\(ng.name)誕生 HP: \(ng.name)")
-        } else {
-            print("HPが不正なので作成失敗しました。")
-        }
-        */
-        
-        
-        //Counter.learn()
-        
-        //Properties.learn()
-        
-        //Enumerations.learn()
-        
-        //Structures.learn()
-        
-        //Closures.learn()
-        
-        
-        //Functions.learn()
-        
-        
-        //ControlFlow.checkAge(age: nil)
-        //ControlFlow.checkAge(age: 10)
-        //ControlFlow.checkAge(age: 18)
-        //ControlFlow.checkAge(age: 20)
-        
-        
-        //ControlFlow.checkPoint((1, 1))
-        //ControlFlow.checkPoint((0, 0))
-        //ControlFlow.checkPoint((0, 3))
-        //ControlFlow.checkPoint((9, 0))
-        //ControlFlow.learn()
-        
-        //CollectionTypes.learn()
-        
-        //Optionals.learn()
-        
-        //Variables.learn()
+        let iphone = Hero(name: "はやと", weapon: Weapon(name: "iPhone", maker: Maker(company: "Apple")))
+        let herosIphone = iphone.weapon?.maker?.company ?? "なんだ？"
+        let ip = iphone.weapon?.maker?.company ?? "どこ？"
+        print("\(iphone.name)の\(herosIphone)の製造元: \(ip)")
     }
 }
 
